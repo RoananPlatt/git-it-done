09c97d8b6afebf4f81e8dcc3a735183c4dc0a1af
Roanan Platt <roanan.platt@gmail.com>
2022-06-02 19:24:49 -0600

    adding messages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -9,6 +10,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
             displayIssues(data);
+
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
         });
         }
         else {
@@ -52,5 +57,18 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo) {
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+};  
 
 getRepoIssues("facebook/react");
